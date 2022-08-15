5efe1c868fb670861cb8bee0a734345b4e1f5afe
Bahri106 <110438429+Bahri106@users.noreply.github.com>
2022-08-15 13:52:29 +0700

    Add files via upload

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY SAIFUL OFC
+
+*🏧 RULES*
+
+1. GUNAKAN BOT SEWAJARNYA 
+	ㅁ [-_-]
+2. INGAT!! BOT PUNYA BUGVN DLL
+	ㅁ [-_-]
+3. MELANGGAR TAU AKIBATNYA
+	ㅁ [-_-]
+4. ANDA SOPAN KAMI SEGAN
+	ㅁ [-_-]
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 6285707645429
+2. PULSA
+	ㅁ 6285853570297
+3. DANA
+	ㅁ 6285707645429
+	
+Semuanya atas nama SAIFUL
+
+*📲 PULSA*
+
+Chat owner
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA DANA/PULSA
+
+*🏧 RULES*
+
+1. GUNAKAN BOT SEWAJARNYA 
+	ㅁ [-_-]
+2. INGAT!! BOT PUNYA BUGVN DLL
+	ㅁ [-_-]
+3. MELANGGAR TAU AKIBATNYA
+	ㅁ [-_-]
+4. ANDA SOPAN KAMI SEGAN
+	ㅁ [-_-]
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 6285707645429
+2. OVO
+	ㅁ 6285707645429
+3. DANA
+	ㅁ 6285707645429
+	
+Semuanya atas nama saiful
+`
+	}
+
+exports.donasibot = donasibot
+ 
